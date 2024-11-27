069b89b9b339ef632356d689a187c2cab5e2cb3f
avarya13 <honor07092021zel@outlook.com>
2024-11-27 18:29:46 +0300

    fix: task 5

--- task_5/task.py
+++ task_5/task.py
@@ -1,34 +1,34 @@
 import numpy as np
 
+
 def build_relation_matrix(A):
     n = sum(len(item) if isinstance(item, list) else 1 for item in A)  # Общее число объектов
     matrix = np.zeros((n, n), dtype=int)  # Инициализируем матрицу нулями
-    idx = 0  # Индекс текущего объекта
+    np.fill_diagonal(matrix, 1)
+    prev_idx = 0
     
     for group in A:
         if isinstance(group, list):  # Если объекты неразличимы
-            for i in range(len(group)):
-                for j in range(len(group)):
-                    matrix[idx + i][idx + j] = 1  # Обозначаем неразличимые объекты
-            idx += len(group)
-        else:
-            matrix[idx][idx] = 1  # Диагональный элемент
-            idx += 1
-    
-    # Заполняем отношения правее стоящих объектов
-    for i in range(n):
-        for j in range(i + 1, n):
-            matrix[i][j] = 1
-            
+            for i in group:
+                for j in group:
+                    matrix[:i, j-1] = 1 
+                    matrix[:j, i-1] = 1
+            prev_idx = max(group) 
+             
+        elif isinstance(group, int):
+            matrix[:prev_idx, group-1] = 1  # Диагональный элемент
+            prev_idx = group
     return matrix
+
   
 def main():
     # Пример входных данных
-    A = [1, 2, 3, 4, 5, 6, 7, [8, 9], 10]
+    A = [1, [2, 3], 4, [5, 6, 7], 8, 9, 10]
     B = [[1, 2], [3, 4, 5], 6, 7, 9, [8, 10]]
 
     # Строим матрицы отношений
     Y_A = build_relation_matrix(A)
+    print()
     Y_B = build_relation_matrix(B)  
 
     print("Матрица отношений Y_A:")
@@ -53,7 +53,9 @@ def main():
 
     # Находим ядро противоречий
     kernel = np.argwhere(result_matrix == 0)
-    filtered_kernel = [[i + 1, j + 1] for i, j in kernel if i < j]
+    filtered_kernel = [[str(i + 1), str(j + 1)] for i, j in kernel if i < j] # Ответы в виде строк
+    #filtered_kernel = [[i + 1, j + 1] for i, j in kernel if i < j]
+
     print("\nЯдро противоречий (уникальные пары):")
     print(filtered_kernel)
 
